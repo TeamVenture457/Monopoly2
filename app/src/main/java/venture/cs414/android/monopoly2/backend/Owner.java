bde3947d8f362f1fa738506a9c91db4213f01e6a
bhamor <ben.hamor94@gmail.com>
2016-11-12 11:42:26 -0700

    Added functionality to Player.  Simplified it a ton from where it was.  Added a constructor to Bank.

--- app/src/main/java/venture/cs414/android/monopoly2/backend/Owner.java
+++ app/src/main/java/venture/cs414/android/monopoly2/backend/Owner.java
@@ -1,9 +1,14 @@
 package venture.cs414.android.monopoly2.backend;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Owner {
 	private List<Property> propertiesOwned;
+
+    public Owner(){
+        propertiesOwned = new ArrayList<Property>();
+    }
 
     public List<Property> getPropertiesOwned() {
         return propertiesOwned;
