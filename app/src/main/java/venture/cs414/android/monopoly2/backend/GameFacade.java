d6d8d1047b7d24f86ef293d3d571d4f56709422d
bhamor <ben.hamor94@gmail.com>
2016-11-12 16:42:03 -0700

    Constructor Stub

---
+++ app/src/main/java/venture/cs414/android/monopoly2/backend/GameFacade.java
@@ -0,0 +1,14 @@
+package venture.cs414.android.monopoly2.backend;
+
+import android.content.Context;
+
+/**
+ * Created by Ben on 11/12/2016.
+ */
+public class GameFacade {
+
+    public GameFacade(int numPlayers, Context context){
+
+    }
+
+}
