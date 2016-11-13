e234a6a7e6a9be853139e6deb8b9515d61ad5781
ajcopley <ajcopley@rams.colostate.edu>
2016-11-12 21:02:46 -0700

    Worked on all GUI's for each view. GUI views are 90% complete. All still need to be hooked up and methods wrote to facade. All buttons are connected.

--- app/src/main/java/venture/cs414/android/monopoly2/frontend/SellProperty.java
+++ app/src/main/java/venture/cs414/android/monopoly2/frontend/SellProperty.java
@@ -1,6 +1,7 @@
 package venture.cs414.android.monopoly2.frontend;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import venture.cs414.android.monopoly2.R;
 
@@ -88,6 +90,16 @@ public class SellProperty extends AppCompatActivity {
 
     public void clickCancelButtonSellProperty(View view){
         //Todo implement code, should pass object back to main and reininitialize that view
+        try {
+            //implement call
+            Intent intent = new Intent(this, MainActivity.class);
+            //Todo need to pass the serializable object when it is created
+            //intent.putExtra("difficulty", 1);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
