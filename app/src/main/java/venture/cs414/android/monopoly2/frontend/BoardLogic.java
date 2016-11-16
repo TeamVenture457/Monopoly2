a52a123f7d0209e31ca6368ead26dba2eb822093
bhamor <ben.hamor94@gmail.com>
2016-11-15 22:24:06 -0700

    Board stuff

---
+++ app/src/main/java/venture/cs414/android/monopoly2/frontend/BoardLogic.java
@@ -0,0 +1,8 @@
+package venture.cs414.android.monopoly2.frontend;
+
+/**
+ * Created by Ben on 11/13/2016.
+ */
+public class BoardLogic {
+
+}
---
+++ app/src/main/java/venture/cs414/android/monopoly2/frontend/BoardSpace.java
@@ -0,0 +1,17 @@
+package venture.cs414.android.monopoly2.frontend;
+
+/**
+ * Created by Ben on 11/13/2016.
+ */
+public class BoardSpace {
+
+    int numberOfPlayers;
+
+    public BoardSpace (int numberOfPlayers){
+        this.numberOfPlayers=numberOfPlayers;
+    }
+
+    public void playersPresent(int[] playerPositions){
+
+    }
+}
