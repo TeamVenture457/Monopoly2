347f0ba19cf175e315c985c7b6a77d0e66919689
bhamor <ben.hamor94@gmail.com>
2016-11-15 22:38:38 -0700

    Buy and sell hotel finished.  Fixed problem in sellhouse

--- app/src/main/java/venture/cs414/android/monopoly2/frontend/SellHouse.java
+++ app/src/main/java/venture/cs414/android/monopoly2/frontend/SellHouse.java
@@ -29,7 +29,7 @@ public class SellHouse extends AppCompatActivity {
 
         gameFacade = GameFacade.getInstance();
 
-        propertySpinner = (Spinner)findViewById(R.id.SelectPropertyDropdownBuyHouse);
+        propertySpinner = (Spinner)findViewById(R.id.SelectPropertyDropdownSellHouse);
         ArrayAdapter<String> propertyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, gameFacade.getStreetsCanSellHouse());
         propertySpinner.setAdapter(propertyAdapter);
     }
