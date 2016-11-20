32d318e39e307f90bd2fbb871e2e1166068d7276
ajcopley <ajcopley@rams.colostate.edu>
2016-11-19 18:58:19 -0700

    fixed error on playerName from merges

--- app/src/main/java/venture/cs414/android/monopoly2/backend/GameFacade.java
+++ app/src/main/java/venture/cs414/android/monopoly2/backend/GameFacade.java
@@ -38,42 +38,41 @@ public class GameFacade {
     public void setUp(int numPlayers, int numMinutes, Context context, boolean radio2, boolean radio3, boolean radio4){
         players = new ArrayList<Player>();
         for (int i = 1; i < numPlayers+1; i++) {
-            String playerName = ("Player " + i);
-            players.add(new Player(playerName, "P" + i));
+            String playerName;
             if(i>1){
                 if(i==2){
                     if (radio2){
-                        String playerName = ("Player " + i);
+                        playerName = ("Player " + i);
                         players.add(new Player(playerName, "P" + i, true));
                     }
                     else{
-                        String playerName = ("Player " + i);
+                        playerName = ("Player " + i);
                         players.add(new Player(playerName, "P" + i, false));
                     }
                 }
                 else if(i==3){
                     if (radio3){
-                        String playerName = ("Player " + i);
+                        playerName = ("Player " + i);
                         players.add(new Player(playerName, "P" + i, true));
                     }
                     else{
-                        String playerName = ("Player " + i);
+                        playerName = ("Player " + i);
                         players.add(new Player(playerName, "P" + i, false));
                     }
                 }
                 else{
                     if (radio4){
-                        String playerName = ("Player " + i);
+                        playerName = ("Player " + i);
                         players.add(new Player(playerName, "P" + i, true));
                     }
                     else{
-                        String playerName = ("Player " + i);
+                        playerName = ("Player " + i);
                         players.add(new Player(playerName, "P" + i, false));
                     }
                 }
             }
             else{
-                String playerName = ("Player " + i);
+                playerName = ("Player " + i);
                 players.add(new Player(playerName, "P" + i, false));
             }
         }
